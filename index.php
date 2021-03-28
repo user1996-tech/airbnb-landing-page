d12c3a169dde125c729591e222b101295d084229
user1996-tech <66292522+user1996-tech@users.noreply.github.com>
2021-03-28 13:13:50 +1100

    first commit

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+      <title>Host your home on Airbnb</title>
+   </head>
+   <header>
+      <!-- Header START --!>
+      <div class="row justify-content-between align-items-center" style="height: 60px;">
+         <div style="padding-left: 30px;"> 
+            <a href="#"><span><img style="height: 35px; width: 35px;" src="./assets/airbnb_logo.png"></img></span></a>
+         </div>
+         <div>
+            <div class="d-inline-block" style="padding-right: 50px;">
+               <a href="#" style="color: #000; text-decoration: underline">Get help from a host</a>
+            </div>
+            <div class="d-inline-block" style="padding-right: 50px;">
+               <div style="padding: 4px 15px 4px 15px; background-color: #e31c5e; border-radius: 10px;"> 
+                  <a class="d-flex align-items-center" style="color: #fff; font-weight: bold;" href="#">Create your listing</a>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- Header END --!>
+   </header>
+   <body> 
+
+      <!-- Section 1 START --!>
+      <div class="row" style="height: 450px;">
+         <img style="height: 35px; width: 35px; margin-top: 20px; margin-left: 40px;" src="./assets/airbnb_logo_white_2.png">
+         <div class="row text-white" style="font-weight: bold; font-size: 15px; position: absolute; left: 75px; top: 160px;">
+            BECOME A HOST
+         </div>
+         <div class="row">
+         </div>
+         <div class="row">
+         </div>
+         <div class="row">
+         </div>
+         <img style="width: 100%; height: 450px; position: absolute; left: 0px; right: 0px; z-index: -1;" src="./assets/airbnb1.jpg">
+      </div>
+      <!-- Section 1 END --!>
+
+      <div class="row bg-success" style="height: 475px;">
+         <p>Section 2 (Your next chapter is made possible by hosting)</p>  
+      </div>
+      <div class="row bg-danger" style="height: 475px;">
+         <p>Section 3 (scrollable banners)</p>  
+      </div>
+      <div class="row bg-warning" style="height: 450px;">
+         <p>Section 4 (Banner see for yourself)</p>  
+      </div>
+      <div class="row bg-info" style="height: 450px;">
+         <p>Section 5 (see how it works)</p>  
+      </div>
+      <div class="row bg-light" style="height: 525px;">
+         <p>Section 6 (how we support you during COVID)</p>  
+      </div>
+      <div class="row bg-dark" style="height: 500px;">
+         <p>Section 7 (start your hosting journey)</p>
+      </div>
+      <div class="row bg-white" style="height: 400px;">
+         <p>Section 8 (find out more)</p>
+      </div>
+      <hr>
+      <div class="row bg-primary" style="height: 450px;">
+         <p>Section 9 (links)</p>
+      </div>
+      <hr>
+   </body>
+   <footer>
+      <div class="row bg-secondary" style="height: 75px;">
+         <p>Footer</p>
+      </div>
+
+   </footer>
+   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> 
+   <script type="text/javascript">
+
+   </script>
+   
+</html>
